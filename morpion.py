b04236c92c5a8116a333edbd6a5e70879606eb46
seif-maachaoui <seifeddine.maachaoui@laplateforme.io>
2023-03-23 12:21:21 +0100

    Ajout d'un message pour annoncer la victoire d'un joueur, avant l'apparition du canvas, je déclare deux variables, une nommée board, et l'autre player. Ensuite je passe à la déclaration des fonctions, je commence par une fonction get_choice pour récupérer le choix de l'utilisateur, puis je déclare une fonction mouse_click, qui va permettre de dessiner des cercles ou des croix lors des click de l'utilisateur, je continue, en déclarant une fonction check_victory, pour vérifier la victoire ou un match nul. Le morpion fonctionne mais il manque les annonces de victoires ainsi que les boutons recommencer et quitter.

--- morpion.py
+++ morpion.py
@@ -1,7 +1,6 @@
 #Je commence par importer le module tkinter ainsi que l'ensemble de ses éléments
 from tkinter import *
 
-
 #-----Création de l'interface graphique-----
 
 window = Tk() #Création d'un objet qui représentera une interface
@@ -11,28 +10,34 @@ window.maxsize(800, 600)#Une taille max de 800px en largeur et 600px de hauteur
 window.minsize(450, 300)#Une taille min de 450px de largeur et 300px de hauteur
 
 #J'ajoute un message de bienvenue pour l'utilisateur
-first_message = Label(window, text = "Bienvenue sur le jeu du morpion")
-first_message.place()
+welcome_message = Label(window, text = "Bienvenue sur le jeu du morpion")
+welcome_message.pack()
 
-make_you_choice = Label(window, text="Veuillez choisir, le cercle ou la croix ?")
+#Je demande à l'utilisateur si il souhaite choisir le cercle ou la croix pour débuter
+make_you_choice = Label(window, text="Vous voulez commencer par le cercle ou la croix ?")
 make_you_choice.place(y=40, x=220)
 
 
 #Ajout de deux boutons, un qui représente le cercle, et l'autre la croix, pour donner le choix au joueur
-first_choice = Button(window, text="O")
-second_choice = Button(window, text="X")
+first_choice = Button(window, text="O", command=lambda: get_choice("O"))
+second_choice = Button(window, text="X", command=lambda: get_choice("X"))
 first_choice.place(y= 80, x = 300)
 second_choice.place(y = 80, x= 330)
 
-#--------Création d'une frame-------
+#Ajout d'un message pour annoncer la victoire d'un joueur
+you_win = Label(window, text= "")
+you_win.place(y = 80, x = 420)
+
+
+# La frame qui va contenir mon canvas
 cadre = Frame(window, bg="grey", width=500, height=500)
 cadre.place(y = 120, x= 190)
 
-#Je vais créer un canvas qui va contenir mon tableau pour le morpion
-tableau = Canvas(cadre, bg="black", width=301, height=301)
-tableau.grid(row=0, column=0, sticky="nsew")
+#Je vais créer un tableau qui prendra la forme d'un Canvas
+tableau = Canvas(cadre, bg="black", width=300, height=300)
+tableau.grid(row=1, column=0, sticky="nsew")
 
-#--------Mise en place de la grille------
+#Je m'occupe de dessiner une grille au sein de mon canvas
 
 #Je trace d'abord 2 lignes verticales
 tableau.create_line(100, 0, 100, 300, width=3, fill="white")
@@ -42,20 +47,72 @@ tableau.create_line(200, 0, 200, 300, width=3, fill="white")
 tableau.create_line(0, 100, 300, 100, width=3, fill="white")
 tableau.create_line(0, 200, 300, 200, width=3, fill="white")
 
-#--------Déclaration des fonctions---------
+# ---- Déclaration des variables -----
 
-def mouse_position(event):
-    x = event.x
-    y = event.y
-    print("Position de la souris: x = {}, y = {}".format(x, y), end="\r")
+#Je souhaite vérifier si une case est vide ou bien déjà occupée avant de pouvoir dessiner dessus
+board = [0]*9
+#Je créer une variable player, que j'initialise à "O"
+player = "O"
 
+#-------- Déclaration des fonctions ---------
+
+#Je souhaite tout d'abord que l'utilisateur puisse soit commencer par le cercle, soit par la croix...
+#Je déclare une première fonction pour récupérer le choix de l'utilisateur
+def get_choice(your_choice): 
+    global player #J'attribue une portée global à ma variable
+
+    if your_choice == "O": #Si l'utilisateur choisi le cercle alors...
+        player = "O"
+    elif your_choice == "X": #Sinon il choisi la croix, alors...
+        player = "X"
+     
+    #En choisisant l'un ou l'autre, les deux boutons vont être desactivés
+    first_choice["state"] = "disabled"
+    second_choice["state"] = "disabled"
+
+#Ensuite je souhaite utiliser les click de la souris pour pouvoir tracer des cercles ou des croix...
 def mouse_click(click):
-    x = click.x
+    global player, board 
+    x = click.x 
     y = click.y
-    print("Clic de souris: x = {}, y = {}".format(x, y), end="\r")
+    line = (y-2)//100
+    column = (x-2)//100
 
+    if board[line*3+column] == 0 :              #On vérifie si les 3 cases dans le tableau est vide
+        if player == "O":                       #Si c'est au tour du cercle, alors...
+            tableau.create_oval(100*column+8, 100*line+8, 100*column+96, 100*line+96, width=2, outline='#e13131')
+            board[line*3+column] = 1            #Si c'est le cas, on dessine le cercle dans la liste
+            player = "X"                        #l'autre joueur dessinera une croix
+        else:                                   #Si c'est au tour de la croix, alors...
+            tableau.create_line(100*column+8, 100*line+8, 100*column+96, 100*line+96, width=2, fill='#3145f0')
+            tableau.create_line(100*column+8, 100*line+96, 100*column+96, 100*line+8, width=2, fill='#3145f0')
+            board[line*3+column] = 2          #Sinon on dessiner une croix
+            player = "O"                        #L'autre joueur dessinera un cercle
 
+#Je déclare une fonction pour vérifier si le joueur remporte la victoire
+def check_victory():
+    global board 
 
-tableau.bind("<Motion>", mouse_position) #Appel de la fonction mouse_position au déplaçement de la souris
+    #Vérification des lignes horizontal
+    for i in range (0, 9, 3):
+        if board[i] == board[i+1] == board[i+2] != 0: #Si une ligne horizontal est remplie, alors..
+            return True
+    
+    #Vérification des lignes vertical
+    for i in range(3):
+        if board[i] == board[i+3] == board[i+6] != 0 : #Si une ligne vertical est remplie, alors...
+            return True
+
+    #Vérification des diagonale de gauche à droite
+    if board[0] == board[4] == board[8] != 0:
+        return True
+    #Vérification de la diagonale de droite à gauche
+    if board[2] == board[4] == board[6] and board[2] != 0:
+        return board[2]
+    
+    #Vérification de match nul 
+    if 0 not in board: #Si toutes les cases sont occupées et qu'aucun joueur n'a gagné, alors...
+        return 0 #match nul
+    return None
 tableau.bind("<Button-1>", mouse_click) #Appel de la fonction mouse_click au click de la souris
 window.mainloop()
